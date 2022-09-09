a1db5a87a065bbab75e8fe76b0cb59f249219224
Hsueh-YANG <prettyjim841231@gmail.com>
2022-09-09 16:50:32 -0400

    Update Csv.py

--- code/Csv.py
+++ code/Csv.py
@@ -1,22 +1,25 @@
 import csv
 
 
-def read_csv(filename):
-    # Open file
-    with open(filename) as file_obj:
-        # Skips the heading
-        # Using next() method
+class Csv:
+    def __init__(self):
+        self.filename = '../data/test.csv'
+        self.count = 0
+        self.data = []
 
-        # Create reader object by passing the file
-        # object to reader method
-        reader_obj = csv.reader(file_obj)
+    def read_csv(self):
+        # Open file
+        with open(self.filename) as file_obj:
+            # Skips the heading
+            # Using next() method
 
-        count = 0
-        data = []
+            # Create reader object by passing the file
+            # object to reader method
+            reader_obj = csv.reader(file_obj)
 
-        # Iterate over each row in the csv file
-        # using reader object
-        for row in reader_obj:
-            data.append(row)
-            count += 1
-        #print(data)
+            # Iterate over each row in the csv file
+            # using reader object
+            for row in reader_obj:
+                self.data.append(row)
+                self.count += 1
+            print(self.data)
