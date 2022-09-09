139229318dd1683bcd0f61b8ed954883ebadb516
Po-Hsun Lin <pohsunlin16@gmail.com>
2022-09-09 14:17:48 -0400

    Update Num.py

--- code/Num.py
+++ code/Num.py
@@ -4,20 +4,15 @@ import statistics
 class num:
     def __init__(self):
         self.lst = []
+        self.n = 0
 
-    def add(self, numbers, limit):
-        for i in range(numbers):
-
-            temp = random.randint(1, numbers)
-
-            if temp in self.lst:
-                continue
-            else:
-                self.lst.append(temp)
-
-            if len(self.lst) > limit:
-                self.lst.pop(random.randint(0, limit - 1))
-        self.lst.sort()
+    def add(self, numbers, limit=500):
+        self.n += 1
+        if self.n <= limit:
+            self.lst.append(numbers)
+        elif random.random() < limit / self.n:
+            pos = random.randint(0, len(self.lst) - 1)
+            self.lst[pos] = numbers
 
     def div(self):
         return statistics.stdev(self.lst)
@@ -25,8 +20,3 @@ class num:
     def mid(self):
         return statistics.median(self.lst)
 
-''' Test
-tt = num()
-tt.add(100)
-print(tt.div(), tt.mid())
-'''
